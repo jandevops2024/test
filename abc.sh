763eea284cbe29d52c5e9f6249b3a9b01ac88c1a
mdhananjaya <m_dhana2003@yahoo.com>
2024-09-04 21:25:49 -0700

    udpated

--- abc.sh
+++ abc.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 echo "This is the abc test file"
 echo "This is the second test file"
-echo "This is for testing webhook"
-echo "Bhaskar and Sravanthi has network issues"
-echo "Hi vasu how are you"
